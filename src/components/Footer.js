2299607d972a119c7381251c881f07d78c73e661
DavidPavach <132976698+DavidPavach@users.noreply.github.com>
2023-06-05 10:47:35 +0100

    Fix: Change The About Link Name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer>
             <p>Copyright &copy; 2023 <span className="font-bold">Priest</span></p>
-            <Link className="text-green-600 hover:text-black" to="/about"> About </Link>
+            <Link className="text-green-600 hover:text-black" to="/about"> About Me </Link>
         </footer>
     )
 }
